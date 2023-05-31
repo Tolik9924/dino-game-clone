6e8d5a9533f5e2d16f7672e463be0904ea684670
Tolik9924 <76564089+Tolik9924@users.noreply.github.com>
2023-05-31 22:12:46 +0300

    make jump dinosaur

---
+++ src/scenes/PlayScene.ts
@@ -0,0 +1,43 @@
+import Phaser from "phaser";
+
+// types
+import { SpriteWithDynamicBody } from "../types";
+
+class PlayScene extends Phaser.Scene {
+
+    player: SpriteWithDynamicBody;
+
+    get gameHeight() {
+        return this.game.config.height as number;
+    }
+
+    constructor() {
+        super("PlayScene");
+    }
+
+    create() {
+        this.createEnvironment();
+        this.createPlayer();
+        this.registerPlayerControl();
+    }
+
+    createEnvironment() {
+        this.add
+            .tileSprite(0, this.gameHeight, 1000, 26, "ground")
+            .setOrigin(0, 1);
+    }
+
+    createPlayer() {
+        this.player = this.physics.add.sprite(0, this.gameHeight, "dino-idle").setOrigin(0, 1);
+    }
+
+    registerPlayerControl() {
+        const spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+        spaceBar.on("down", () => {
+            this.player.setVelocityY(-1600);
+        });
+    }
+}
+
+export default PlayScene;
